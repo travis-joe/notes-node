4dda951fa70266cfba7698ea3c239d5fe712c790
Travis Joe <weelgod@gmail.com>
2017-04-04 17:40:16 +0800

    adding & saving notes

--- notes.js
+++ notes.js
@@ -1,5 +1,5 @@
 console.log('starting notes.js');
-
+const fs = require('fs');
 // module.exports.addNotes = () => {
 //     console.log('addNotes');
 //     return 'New note';
@@ -8,7 +8,27 @@ console.log('starting notes.js');
 // module.exports.add = (a, b) => a + b;
 
 const addNote = (title, body) => {
-    console.log(title,body)
+    let notes = [];
+    let note = {
+        title,
+        body
+    };
+    try{
+        //read JSON
+        let noteString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(noteString);
+    }catch(e) {
+
+    }
+
+    const duplicateNotes = notes.filter( (note) => note.title === title );
+    if(duplicateNotes.length === 0) {
+        //add to notes
+        notes.push(note);
+        //write JSON
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
 };
 
 const getAll = () => {
@@ -19,5 +39,5 @@ const getNote = (title) => {
 };
 const removeNote = (title) => {
     console.log(`Removing note ${title}`)
-}
+};
 module.exports  = { addNote, getAll, getNote, removeNote};
